55149b8e5370a05eecfeefd9a892491d3c018d13
miu oohira <miu.000408@gmail.com>
2021-02-12 14:28:25 +0900

    ツイート画面作成[設定]

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,16 @@
+class BlogsController < ApplicationController
+  def index
+    @blogs =Blog.all
+  end
+  def new
+    @blog= Blog.new
+  end
+  def create
+    # Blog.create(blog_params)
+    redirect_to new_blog_path
+  end
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+end
